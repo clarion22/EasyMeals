26ca32a8b18c9460f123a1eebd207bea830d0920
Olga Thompson <olgatj2007@gmail.com>
2021-03-03 20:01:07 -0500

    layout for foodgroups

--- react-app/src/components/CreatePlate/index.js
+++ react-app/src/components/CreatePlate/index.js
@@ -2,16 +2,43 @@ import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import CalorieForm from '../CalorieForm';
-
+import './createplate.css'
 
 function CreatePlate() {
   return (
     <div>
       <Grid container spacing={2}>
         <Grid item container direction="column" xs={6}>
-          <Paper>xs 2</Paper>
-          <Paper>xs 2</Paper>
-          <Paper>xs 2</Paper>
+          <Paper style={{height: '400px'}} className="circle">
+            <span className="dot"></span>
+          </Paper>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <Paper>
+                <h6>Fruit</h6>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+            <Paper>
+              <h6>Carbs</h6>
+            </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>
+                <h6>Protein</h6>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+            <Paper>
+              <h6>Vegetables</h6>
+            </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>
+                <h6>Dairy</h6>
+              </Paper>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item container direction="column" xs={6}>
           <Paper>
