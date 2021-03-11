8f398baa5832f2e2322e6e0e3f748256f8b97bc4
Olga Thompson <olgatj2007@gmail.com>
2021-03-10 23:05:04 -0500

    doughnut chart interactive

--- react-app/src/components/Graphs/DoughnutChart/index.js
+++ react-app/src/components/Graphs/DoughnutChart/index.js
@@ -1,24 +1,30 @@
 import React, {useState, useEffect} from 'react'
 import { Doughnut } from 'react-chartjs-2';
-
+import {useSelector} from 'react-redux';
 
 function DoughnutChart() {
 const [chartData, setChartData] = useState({});
+const [calorieData, setCalorieData] = useState([0, 0, 0, 1, 1])
+const selectedProtein = useSelector(state => state.recipe.selected.Protein)
+const selectedVegetables = useSelector(state => state.recipe.selected.Vegetables)
+const selectedFruit = useSelector(state => state.recipe.selected.Fruit)
+const selectedDairy = useSelector(state => state.recipe.selected.Dairy)
+const selectedCarb = useSelector(state => state.recipe.selected.Carbs)
 
 const createChart = () => {
   setChartData({
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Protein', 'Dairy', 'Vegetables', 'Fruit', 'Carbs'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: calorieData,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(255, 206, 86, 0.8)',
+        'rgba(75, 192, 192, 0.8)',
+        'rgba(153, 102, 255, 0.8)',
+        'rgba(255, 159, 64, 0.8)',
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
@@ -35,8 +41,15 @@ const createChart = () => {
 }
 
 useEffect(() => {
+   if (selectedProtein || selectedVegetables || selectedFruit || selectedDairy || selectedCarb) {
+   setCalorieData([selectedProtein.calories, selectedDairy.calories, selectedVegetables.calories, selectedFruit.calories, selectedCarb.calories])
+   }
+}, [selectedProtein, selectedVegetables, selectedFruit, selectedDairy, selectedCarb])
+
+useEffect(() => {
   createChart()
-},[])
+},[calorieData])
+
   return (
     <div>
       <Doughnut data={chartData} options={{responsive: true, maintainAspectRatio: false}} />
